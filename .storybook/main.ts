f196fbaf953618882df3e197eacef25735770bf8
Konstantin <konstantin.miheev@sensearena.com>
2023-07-04 14:54:21 +0200

    update storybook build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -10,5 +11,11 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   staticDirs: [{ from: '../imgs', to: '/imgs' }],
+  async viteFinal(config) {
+    console.debug(config.mode);
+    return mergeConfig(config, {
+      base: config.mode === 'production' ? '/match-visualization/' : '/',
+    });
+  },
 };
 export default config;
