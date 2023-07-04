f4b563cb6e348804b90270d9f3666eb77df8a3a8
Konstantin <konstantin.miheev@sensearena.com>
2023-07-04 15:02:31 +0200

    removed storybook deploy

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -9,13 +8,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
-  },
-  staticDirs: [{ from: '../imgs', to: '/imgs' }],
-  async viteFinal(config) {
-    console.debug(config.mode);
-    return mergeConfig(config, {
-      base: config.mode === 'production' ? '/match-visualization/' : '/',
-    });
   },
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -43,5 +43,4 @@ export default defineConfig(({ mode }) => ({
       external: [...Object.keys(peerDependencies)],
     },
   },
-  mode,
 }));
